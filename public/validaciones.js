a0d6d29db51743e6b3bad63984e32a1a9e2069ed
Tito Berru <c_myco@hotmail.com>
2021-10-15 11:38:16 -0300

    proyecto mercadoLiebre para codo a codo

---
+++ public/validaciones.js
@@ -0,0 +1,17 @@
+validacionesFront = window.addEventListener('load', function () {
+    let btnVender = document.querySelector('.vender');
+    btnVender.addEventListener('click', function () {
+        alert("Funcionalidad aún no desarrollada")
+    }
+    );
+    let btnAyuda = document.querySelector('.ayuda');
+    btnAyuda.addEventListener('click', function () {
+        alert("Funcionalidad aún no desarrollada")
+    }
+    );
+     let btnmisCompras = document.querySelector('.misCompras');
+    btnmisCompras.addEventListener('click', function () {
+        alert("Funcionalidad aún no desarrollada")
+    }
+    );
+})
